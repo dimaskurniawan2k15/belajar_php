dd48248bdae0579a7db7cc61223e51f7e4dff031
dimaskurniawan2k15 <dimas.kurniawan11012004@gmail.com>
2022-03-11 13:17:44 +0700

    belajar

---
+++ pertemuan2/index.php
@@ -0,0 +1,70 @@
+<?php 
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output :
+// echo, print
+// print_r
+// var_dump = untuk debugging
+
+// echo "Dimas Kurniawan";
+
+// Penulisan sintaks PHP :
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan tipe data
+// Variabel :
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Dimas Kurniawan";
+
+// Operator 
+// aritmatika :
+// + - * / %
+// $x = 10;
+// $y = 20;
+
+// echo $x * $y;
+
+// penggabung string / concatenation / concat
+// // .
+// $nama_depan = "Dimas";
+// $nama_belakang = "Kurniawan";
+
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assigment
+// = += -= *= /= %= .=
+// $x = 1;
+// $x -= 5;
+// echo $x;
+// $nama = "Dimas";
+// $nama .= " ";
+// $nama .= "Kurniawan";
+// echo $nama;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+
+// identitas
+//  ===, !==
+
+// Logika
+// &&, ||, !
+
+?>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Halo, selamat datang <?php echo "Dimas"; ?></h1>
+    <h1>Halo, selamat datang <?php echo $nama; ?></h1>
+    <p><?php echo "ini adalah paragraf"; ?></p>
+</body>
+</html> -->
